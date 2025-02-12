8bcc19211dce12371fd15798713fbea5fc7c5a12
RandSlot <dunin.stas@gmail.com>
2025-02-12 09:02:30 +0300

    script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 document.addEventListener("DOMContentLoaded", function () {
     let playButton = document.getElementById("playButton");
 
-    // Обработчик для нажатия на ссылку
+    // Обработчик клика по кнопке
     playButton.addEventListener("click", function (event) {
-        event.preventDefault(); // Отменяем стандартное поведение ссылки
+        event.preventDefault(); 
 
         let sound = new Audio('click.mp3');  
         sound.play().catch(() => console.log("Автовоспроизведение заблокировано")); 
@@ -11,6 +11,6 @@ document.addEventListener("DOMContentLoaded", function () {
         // Переход на сайт после проигрывания звука
         setTimeout(() => {
             window.location.href = "https://1wcneg.com/casino/list?open=register&p=o0qr";
-        }, 1000); // Переход через 1 секунду после проигрывания звука
+        }, 1000);
     });
 });
